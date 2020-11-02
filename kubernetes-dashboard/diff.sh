fe07f0d28ed4db6a127eff339546bfc74d843773
Fabio Berchtold <jamesclonk@jamesclonk.ch>
2020-11-02 16:12:16 +0100

    Update diff.sh

--- kubernetes-dashboard/diff.sh
+++ kubernetes-dashboard/diff.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
+set -u
 source ../setup.sh
 
 # diff
 kapp app-change list -a kubernetes-dashboard
+DASHBOARD_BASIC_AUTH=$(basic_auth ${DASHBOARD_BASIC_AUTH_USERNAME} ${DASHBOARD_BASIC_AUTH_PASSWORD})
 cat values.yml | envsubst -no-unset -no-empty | ytt --ignore-unknown-comments -f templates -f - | kapp deploy -a kubernetes-dashboard -c --diff-run -f -
