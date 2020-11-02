95834d2ce739b000c401a3615ae08a5f51a293b0
Fabio Berchtold <jamesclonk@jamesclonk.ch>
2020-11-02 16:11:00 +0100

    add basic_auth

--- kubernetes-dashboard/deploy.sh
+++ kubernetes-dashboard/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
+set -u
 source ../setup.sh
 
 # deploy
 echo "deploying [kubernetes-dashboard] ..."
+DASHBOARD_BASIC_AUTH=$(basic_auth ${DASHBOARD_BASIC_AUTH_USERNAME} ${DASHBOARD_BASIC_AUTH_PASSWORD})
 cat values.yml | envsubst -no-unset -no-empty | ytt --ignore-unknown-comments -f templates -f - | kapp -y deploy -a kubernetes-dashboard -c -f -
 kapp -y app-change garbage-collect -a kubernetes-dashboard --max 5
 echo "done!"
