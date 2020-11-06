65edfe9ea3d98955b0cfde8fe0477828318e42d5
JamesClonk <jamesclonk@jamesclonk.ch>
2020-11-06 12:05:48 +0100

    chmod +x

--- kubernetes-dashboard/diff.sh
+++ kubernetes-dashboard/diff.sh
@@ -5,7 +5,7 @@ source ../setup.sh
 
 # diff
 kapp app-change list -a kubernetes-dashboard
-INGRESS_BASIC_AUTH=$(basic_auth "${INGRESS_BASIC_AUTH_USERNAME}" "${INGRESS_BASIC_AUTH_PASSWORD}")
+export INGRESS_BASIC_AUTH=$(basic_auth "${INGRESS_BASIC_AUTH_USERNAME}" "${INGRESS_BASIC_AUTH_PASSWORD}")
 cat values.yml |
 	envsubst -no-unset -no-empty |
 	ytt --ignore-unknown-comments -f templates -f - |
