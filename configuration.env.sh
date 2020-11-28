af44b7f12dd64b19aaff40d8f0deb06aa98a402d
JamesClonk <jamesclonk@jamesclonk.ch>
2020-11-28 13:56:12 +0100

    update workflows

--- configuration.env.sh
+++ configuration.env.sh
@@ -8,10 +8,10 @@ export KUBECONFIG="$HOME/.kube/hetzner-k3s"
 ########################################################################################################################
 ####### hetzner cloud - k3s ############################################################################################
 ########################################################################################################################
-export HCLOUD_TOKEN="${HCLOUD_TOKEN}" # https://docs.hetzner.cloud/#getting-started
+export HCLOUD_TOKEN="${HCLOUD_TOKEN:=invalid}" # https://docs.hetzner.cloud/#getting-started
 
 export HETZNER_SSH_KEY_NAME="kubernetes"
-export HETZNER_PUBLIC_SSH_KEY="${HETZNER_PUBLIC_SSH_KEY}" # your _public_ ssh-key
+export HETZNER_PUBLIC_SSH_KEY="${HETZNER_PUBLIC_SSH_KEY:=invalid}" # your _public_ ssh-key
 
 export HETZNER_PRIVATE_NETWORK_NAME="k8s-private"
 export HETZNER_PRIVATE_NETWORK_RANGE="10.8.0.0/16"
@@ -37,16 +37,16 @@ export HETZNER_K3S_VERSION="v1.19.4+k3s1" # https://github.com/rancher/k3s/relea
 ########################################################################################################################
 ####### ingress-nginx ##################################################################################################
 ########################################################################################################################
-export INGRESS_DOMAIN="${INGRESS_DOMAIN}" # for example "mydomain.org"
-export INGRESS_BASIC_AUTH_USERNAME="${INGRESS_BASIC_AUTH_USERNAME}" # for example "my-username"
-export INGRESS_BASIC_AUTH_PASSWORD="${INGRESS_BASIC_AUTH_PASSWORD}" # for example "my-super-secret-password"
+export INGRESS_DOMAIN="${INGRESS_DOMAIN:=example.org}" # for example "mydomain.org"
+export INGRESS_BASIC_AUTH_USERNAME="${INGRESS_BASIC_AUTH_USERNAME:=username}" # for example "my-username"
+export INGRESS_BASIC_AUTH_PASSWORD="${INGRESS_BASIC_AUTH_PASSWORD:=not-secret}" # for example "my-super-secret-password"
 ########################################################################################################################
 
 
 ########################################################################################################################
 ####### cert-manager ###################################################################################################
 ########################################################################################################################
-export LETS_ENCRYPT_EMAIL_ADDRESS="${LETS_ENCRYPT_EMAIL_ADDRESS}" # for example "admin@mydomain.org"
+export LETS_ENCRYPT_EMAIL_ADDRESS="${LETS_ENCRYPT_EMAIL_ADDRESS:=nobody@nobody}" # for example "admin@mydomain.org"
 export LETS_ENCRYPT_PROD_KEY="${LETS_ENCRYPT_PROD_KEY:=NONE}" # base64 encoded tls key
 export LETS_ENCRYPT_STAGING_KEY="${LETS_ENCRYPT_STAGING_KEY:=NONE}" # base64 encoded tls key
 ########################################################################################################################
@@ -68,6 +68,6 @@ export PROMETHEUS_ALERTMANAGER_EMAIL_HOST="${PROMETHEUS_ALERTMANAGER_EMAIL_HOST:
 export PROMETHEUS_ALERTMANAGER_EMAIL_PASSWORD="${PROMETHEUS_ALERTMANAGER_EMAIL_PASSWORD:=my-password}"
 export PROMETHEUS_ALERTMANAGER_EMAIL_FROM="${PROMETHEUS_ALERTMANAGER_EMAIL_FROM:=nobody@nobody}"
 export PROMETHEUS_ALERTMANAGER_EMAIL_TO="${PROMETHEUS_ALERTMANAGER_EMAIL_TO:=nobody@nobody}"
-export PROMETHEUS_ALERTMANAGER_TEAMS_WEBHOOK_ENABLED="${PROMETHEUS_ALERTMANAGER_TEAMS_WEBHOOK_ENABLED:=false}" # false or true
+export PROMETHEUS_ALERTMANAGER_TEAMS_WEBHOOK_ENABLED="${PROMETHEUS_ALERTMANAGER_TEAMS_WEBHOOK_ENABLED:=true}" # false or true
 export PROMETHEUS_ALERTMANAGER_TEAMS_WEBHOOK_URL="${PROMETHEUS_ALERTMANAGER_TEAMS_WEBHOOK_URL:=http://example/deadbeef}" # https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook
 ########################################################################################################################
