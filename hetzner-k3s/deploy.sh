3ca3389c17f2c803ebf28f3015dd7ab8237789b3
JamesClonk <jamesclonk@jamesclonk.ch>
2020-12-02 14:36:34 +0100

    reenable ufw

--- hetzner-k3s/deploy.sh
+++ hetzner-k3s/deploy.sh
@@ -101,6 +101,8 @@ hcloud server ssh -p "${HETZNER_SSH_PORT}" "${HETZNER_NODE_NAME}" \
 	  && ufw allow 6443/tcp \
 	  && ufw allow 32222/tcp \
 	  && ufw allow 32222/udp \
+	  && ufw logging off \
+	  && ufw disable \
 	  && sleep 2 \
 	  && ufw --force enable"
 hcloud server ssh -p "${HETZNER_SSH_PORT}" "${HETZNER_NODE_NAME}" "sleep 2 && ufw reload; sleep 2 && ufw status"
