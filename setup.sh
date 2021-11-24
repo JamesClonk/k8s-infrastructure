dde55c21a8f5368fe86d260c27e8e39b076cc962
JamesClonk <jamesclonk@jamesclonk.ch>
2021-11-24 11:33:29 +0100

    update kbld

--- setup.sh
+++ setup.sh
@@ -64,7 +64,7 @@ install_tool "kubectl" "https://storage.googleapis.com/kubernetes-release/releas
 install_tool "kapp" "https://github.com/k14s/kapp/releases/download/v0.35.0/kapp-linux-amd64" "0f9d4daa8c833a8e245362c77e72f4ed06d4f0a12eed6c09813c87a992201676"
 install_tool "ytt" "https://github.com/k14s/ytt/releases/download/v0.31.0/ytt-linux-amd64" "32e7cdc38202b49fe673442bd22cb2b130e13f0f05ce724222a06522d7618395"
 install_tool "vendir" "https://github.com/k14s/vendir/releases/download/v0.23.0/vendir-linux-amd64" "c6a65e7e8e589e25bf5554e9575ab247e55ae71920d3d366ffd84b1571fe34ac"
-install_tool "kbld" "https://github.com/k14s/kbld/releases/download/v0.29.0/kbld-linux-amd64" "28492a398854e8fec7dd9537243b07af7f43e6598e1e4557312f5481f6840499"
+install_tool "kbld" "https://github.com/k14s/kbld/releases/download/v0.31.0/kbld-linux-amd64" "ba0be56d9e74b067f3e659de0b79100b0b9df86a2e3e0e6ff533b1e019c22c23"
 install_tool "kind" "https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64" "949f81b3c30ca03a3d4effdecda04f100fa3edc07a28b19400f72ede7c5f0491"
 install_tool_from_tarball "hcloud" "hcloud" "https://github.com/hetznercloud/cli/releases/download/v1.23.0/hcloud-linux-amd64.tar.gz" "500320950002dd9d24eeb47c66b3136c5318fa08c5f73e1b981a16a4dc320cad"
 install_tool_from_tarball "linux-amd64/helm" "helm" "https://get.helm.sh/helm-v3.4.0-linux-amd64.tar.gz" "58550525963821a227307590627d0c266414e4c56247a5c11559e4abd990b0ae"
