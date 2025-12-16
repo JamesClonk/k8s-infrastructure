379ade655a46b25b0ebf2eae6985a2a725374558
JamesClonk <jamesclonk@jamesclonk.ch>
2025-12-16 10:28:06 +0100

    update tooling [ci skip]

--- setup.sh
+++ setup.sh
@@ -15,44 +15,56 @@ export KUBECONFIG="$HOME/.kube/k8s-infrastructure"
 # helper functions
 ########################################################################################################################
 function basic_auth() {
-	local -r username="$1"; shift
-	local -r password="$1"; shift
+	local -r username="$1"
+	shift
+	local -r password="$1"
+	shift
 	echo "${username}:$(openssl passwd -apr1 \"${password}\")"
 }
 
 function retry() {
-	local -r -i max_attempts="$1"; shift
-	local -r -i sleep_time="$1"; shift
+	local -r -i max_attempts="$1"
+	shift
+	local -r -i sleep_time="$1"
+	shift
 	local -i attempt_num=1
 	until "$@"; do
-		if (( attempt_num == max_attempts ))
-	then
-		echo "#$attempt_num failures!"
-		exit 1
-	else
-		echo "#$(( attempt_num++ )): trying again in $sleep_time seconds ..."
-		sleep $sleep_time
+		if ((attempt_num == max_attempts)); then
+			echo "#$attempt_num failures!"
+			exit 1
+		else
+			echo "#$((attempt_num++)): trying again in $sleep_time seconds ..."
+			sleep $sleep_time
 		fi
 	done
 }
 
 function install_tool {
-	local -r tool_name="$1"; shift
-	local -r tool_url="$1"; shift
-	local -r tool_checksum="$1"; shift
+	local -r tool_name="$1"
+	shift
+	local -r tool_url="$1"
+	shift
+	local -r tool_checksum="$1"
+	shift
 	if [ ! -f "$HOME/bin/${tool_name}" ]; then
 		echo "downloading [${tool_name}] ..."
 		wget -q "${tool_url}" -O "$HOME/bin/${tool_name}" 2>/dev/null
 		chmod +x "$HOME/bin/${tool_name}"
+
+		found_checksum=$(sha256sum -z "$HOME/bin/${tool_name}" | awk '{print $1;}')
+		echo "${found_checksum}" | grep "${tool_checksum}" >/dev/null || (echo "checksum failed for [${tool_name}], it was [${found_checksum}] instead of the expected [${tool_checksum}]" && rm -f "$HOME/bin/${tool_name}" && exit 1)
 	fi
-	sha256sum "$HOME/bin/${tool_name}" | grep "${tool_checksum}" >/dev/null || (echo "checksum failed for [${tool_name}]" && rm -f "$HOME/bin/${tool_name}" && exit 1)
 }
 
 function install_tool_from_tarball {
-	local -r tool_path="$1"; shift
-	local -r tool_name="$1"; shift
-	local -r tool_url="$1"; shift
-	local -r tool_checksum="$1"; shift
+	local -r tool_path="$1"
+	shift
+	local -r tool_name="$1"
+	shift
+	local -r tool_url="$1"
+	shift
+	local -r tool_checksum="$1"
+	shift
 	if [ ! -f "$HOME/bin/${tool_name}" ]; then
 		echo "downloading [${tool_name}] ..."
 		wget -q "${tool_url}" -O "$HOME/bin/${tool_name}.tgz" 2>/dev/null
@@ -61,8 +73,10 @@ function install_tool_from_tarball {
 		tar -xvzf "$HOME/bin/${tool_name}.tgz" --strip-components=${STRIP_COMPONENTS} -C "$HOME/bin/" "${tool_path}" >/dev/null
 		chmod +x "$HOME/bin/${tool_name}"
 		rm -f "$HOME/bin/${tool_name}.tgz"
+
+		found_checksum=$(sha256sum -z "$HOME/bin/${tool_name}" | awk '{print $1;}')
+		echo "${found_checksum}" | grep "${tool_checksum}" >/dev/null || (echo "checksum failed for [${tool_name}], it was [${found_checksum}] instead of the expected [${tool_checksum}]" && rm -f "$HOME/bin/${tool_name}" && exit 1)
 	fi
-	sha256sum "$HOME/bin/${tool_name}" | grep "${tool_checksum}" >/dev/null || (echo "checksum failed for [${tool_name}]" && rm -f "$HOME/bin/${tool_name}" && exit 1)
 }
 
 ########################################################################################################################
@@ -74,21 +88,21 @@ export PATH="$HOME/bin:$PATH"
 ########################################################################################################################
 # install tools
 ########################################################################################################################
-install_tool "sops" "https://github.com/mozilla/sops/releases/download/v3.7.3/sops-v3.7.3.linux.amd64" "53aec65e45f62a769ff24b7e5384f0c82d62668dd96ed56685f649da114b4dbb"
-install_tool "kubectl" "https://storage.googleapis.com/kubernetes-release/release/v1.25.8/bin/linux/amd64/kubectl" "80e70448455f3d19c3cb49bd6ff6fc913677f4f240d368fa2b9f0d400c8cd16e"
-install_tool "kapp" "https://github.com/vmware-tanzu/carvel-kapp/releases/download/v0.46.0/kapp-linux-amd64" "130f648cd921761b61bb03d7a0f535d1eea26e0b5fc60e2839af73f4ea98e22f"
-install_tool "ytt" "https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.40.1/ytt-linux-amd64" "11222665c627b8f0a1443534a3dde3c9b3aac08b322d28e91f0e011e3aeb7df5"
-install_tool "vendir" "https://github.com/vmware-tanzu/carvel-vendir/releases/download/v0.26.0/vendir-linux-amd64" "98057bf90e09972f156d1c4fbde350e94133bbaf2e25818b007759f5e9c8b197"
-install_tool "kbld" "https://github.com/vmware-tanzu/carvel-kbld/releases/download/v0.32.0/kbld-linux-amd64" "de546ac46599e981c20ad74cd2deedf2b0f52458885d00b46b759eddb917351a"
-install_tool "imgpkg" "https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.25.0/imgpkg-linux-amd64" "14ce0b48a3a00352cdf0ef263aa98a9bcd90d5ea8634fdf6b88016e2a08f09d1"
-install_tool "yq" "https://github.com/mikefarah/yq/releases/download/v4.21.1/yq_linux_amd64" "50778261e24c70545a3ff8624df8b67baaff11f759e6e8b2e4c9c781df7ea8dc"
-install_tool "jq" "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" "af986793a515d500ab2d35f8d2aecd656e764504b789b66d7e1a0b727a124c44"
-install_tool "kind" "https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64" "949f81b3c30ca03a3d4effdecda04f100fa3edc07a28b19400f72ede7c5f0491"
-install_tool_from_tarball "cmctl" "cmctl" "https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cmctl-linux-amd64.tar.gz" "6e22fda56e0fa62cb3fab9340be23ba3fa0da341e737006a9552abdcc80c789e"
-install_tool_from_tarball "hcloud" "hcloud" "https://github.com/hetznercloud/cli/releases/download/v1.30.3/hcloud-linux-amd64.tar.gz" "0e6c2c1bf8be9747059c811004ba37bba884b4b194d8f3e0262cbab6c1c0e339"
-install_tool_from_tarball "linux-amd64/helm" "helm" "https://get.helm.sh/helm-v3.10.3-linux-amd64.tar.gz" "cc5223b23fd2ccdf4c80eda0acac7a6a5c8cdb81c5b538240e85fe97aa5bc3fb"
-install_tool_from_tarball "age/age" "age" "https://github.com/FiloSottile/age/releases/download/v1.1.1/age-v1.1.1-linux-amd64.tar.gz" "0c6ddc31c276f55e9414fe27af4aada4579ce2fb824c1ec3f207873a77a49752"
-install_tool_from_tarball "age/age-keygen" "age-keygen" "https://github.com/FiloSottile/age/releases/download/v1.1.1/age-v1.1.1-linux-amd64.tar.gz" "e279f64ccd11347e57b8d28304e3e358ae1a5ef4f19107e7a1f9c9156fdcad91"
+install_tool "sops" "https://github.com/getsops/sops/releases/download/v3.11.0/sops-v3.11.0.linux.amd64" "775f1384d55decfad228e7196a3f683791914f92a473f78fc47700531c29dfef"
+install_tool "kubectl" "https://dl.k8s.io/release/v1.34.2/bin/linux/amd64/kubectl" "9591f3d75e1581f3f7392e6ad119aab2f28ae7d6c6e083dc5d22469667f27253"
+install_tool "kapp" "https://github.com/carvel-dev/kapp/releases/download/v0.65.0/kapp-linux-amd64" "9cb88745d189bbfe2423771d68f50f7222ca33187350470857cca124d3341233"
+install_tool "ytt" "https://github.com/carvel-dev/ytt/releases/download/v0.52.1/ytt-linux-amd64" "490f138ae5b6864071d3c20a5a231e378cee7487cd4aeffc79dbf66718e65408"
+install_tool "vendir" "https://github.com/carvel-dev/vendir/releases/download/v0.45.0/vendir-linux-amd64" "d60ad65bbd0658d377f2dcf57b3119f16c5a3a7eeaf80019a3d243a620404d7e"
+install_tool "kbld" "https://github.com/carvel-dev/kbld/releases/download/v0.47.0/kbld-linux-amd64" "f9cf1d84ed8dd7c19133044e15939e62c9929ecf1115edeb7275f45b99e2d1ac"
+install_tool "imgpkg" "https://github.com/carvel-dev/imgpkg/releases/download/v0.47.0/imgpkg-linux-amd64" "7602b6af24a818265dcb2cc0dc7f6117a3591f26e2c266f294800f99ae433da1"
+install_tool "yq" "https://github.com/mikefarah/yq/releases/download/v4.49.2/yq_linux_amd64" "be2c0ddcf426b6a231648610ec5d1666ae50e9f6473e82f6486f9f4cb6e3e2f7"
+install_tool "jq" "https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-amd64" "020468de7539ce70ef1bceaf7cde2e8c4f2ca6c3afb84642aabc5c97d9fc2a0d"
+install_tool "kind" "https://github.com/kubernetes-sigs/kind/releases/download/v0.30.0/kind-linux-amd64" "517ab7fc89ddeed5fa65abf71530d90648d9638ef0c4cde22c2c11f8097b8889"
+install_tool "cmctl" "https://github.com/cert-manager/cmctl/releases/download/v2.4.0/cmctl_linux_amd64" "57d12fc2c5fd7a9f2df372130c7da7f995855a06058ca41a2b6c66c26a6ca7ae"
+install_tool_from_tarball "hcloud" "hcloud" "https://github.com/hetznercloud/cli/releases/download/v1.57.0/hcloud-linux-amd64.tar.gz" "6852b9dde7e5d413c734d4ae134f631c2ec7380d4a6ad524227f42dd5ecdcd23"
+install_tool_from_tarball "linux-amd64/helm" "helm" "https://get.helm.sh/helm-v3.19.3-linux-amd64.tar.gz" "39244958465c4be60f3815b3cf5ddc1904024cef3753bfd54667bfaefd8a0d1b"
+install_tool_from_tarball "age/age" "age" "https://github.com/FiloSottile/age/releases/download/v1.2.1/age-v1.2.1-linux-amd64.tar.gz" "aaec874ed903da4b02a9d503778ae05ee5005b2acc0f4a4cf10e5d0f17fd4384"
+install_tool_from_tarball "age/age-keygen" "age-keygen" "https://github.com/FiloSottile/age/releases/download/v1.2.1/age-v1.2.1-linux-amd64.tar.gz" "50f849de5f05dc136de3dba623dd2b7e166e7badb9d4e3d83da88f2dbdb2d439"
 
 ########################################################################################################################
 # git config
@@ -140,7 +154,7 @@ if [ ! -d "$HOME/.ssh" ]; then mkdir "$HOME/.ssh"; fi
 chmod 700 "$HOME/.ssh" || true
 set +u
 if [ ! -z "${HETZNER_PRIVATE_SSH_KEY}" ]; then
-	cat > "$HOME/.ssh/id_rsa" << EOF
+	cat >"$HOME/.ssh/id_rsa" <<EOF
 ${HETZNER_PRIVATE_SSH_KEY}
 EOF
 	chmod 600 "$HOME/.ssh/id_rsa"
@@ -152,8 +166,8 @@ set -o pipefail
 # known_hosts
 ########################################################################################################################
 if [ ! -f "$HOME/.ssh/known_hosts" ]; then
-	hcloud server list -o noheader | grep "${HETZNER_NODE_NAME}" 1>/dev/null \
-		&& ssh-keyscan -p "${HETZNER_SSH_PORT}" "$(hcloud server ip "${HETZNER_NODE_NAME}")" 2>/dev/null >> "$HOME/.ssh/known_hosts" || true
+	hcloud server list -o noheader | grep "${HETZNER_NODE_NAME}" 1>/dev/null &&
+		ssh-keyscan -p "${HETZNER_SSH_PORT}" "$(hcloud server ip "${HETZNER_NODE_NAME}")" 2>/dev/null >>"$HOME/.ssh/known_hosts" || true
 	chmod 600 "$HOME/.ssh/known_hosts" || true
 fi
 
@@ -165,9 +179,9 @@ chmod 700 "$HOME/.kube" || true
 set +u
 if [ ! -f "${KUBECONFIG}" ]; then
 	echo "writing [${KUBECONFIG}] ..."
-	hcloud server list -o noheader | grep "${HETZNER_NODE_NAME}" 1>/dev/null \
-		&& hcloud server ssh -p "${HETZNER_SSH_PORT}" "${HETZNER_NODE_NAME}" \
-		'cat /etc/rancher/k3s/k3s.yaml' | sed "s/127.0.0.1/${INGRESS_DOMAIN}/g" > "${KUBECONFIG}" || true
+	hcloud server list -o noheader | grep "${HETZNER_NODE_NAME}" 1>/dev/null &&
+		hcloud server ssh -p "${HETZNER_SSH_PORT}" "${HETZNER_NODE_NAME}" \
+			'cat /etc/rancher/k3s/k3s.yaml' | sed "s/127.0.0.1/${INGRESS_DOMAIN}/g" >"${KUBECONFIG}" || true
 	chmod 600 "${KUBECONFIG}"
 fi
 set -u
