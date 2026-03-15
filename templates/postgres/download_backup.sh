f1bbb49bf78585c8bbdd6df9210abdd7abbd0c4a
JamesClonk <jamesclonk@jamesclonk.ch>
2026-03-15 17:33:38 +0100

    add script to download backups

---
+++ templates/postgres/download_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+source ../setup.sh
+
+# vars
+export S3_ACCESS_KEY="{{{ .postgres.backup.s3.access_key_id }}}"
+export S3_SECRET_KEY="{{{ .postgres.backup.s3.secret_access_key }}}"
+export S3_ENDPOINT="{{{ .postgres.backup.s3.endpoint }}}"
+export S3_BUCKET="{{{ .postgres.backup.s3.bucket }}}"
+
+# download
+set +x
+mc alias set postgres_backup "${S3_ENDPOINT}" "${S3_ACCESS_KEY}" "${S3_SECRET_KEY}" --api S3v2
+echo "downloading latest backup of [postgres] from [${S3_ENDPOINT}/${S3_BUCKET}] ..."
+mc ls postgres_backup/${S3_BUCKET}/pgbackup/
+mc ls --json postgres_backup/${S3_BUCKET}/pgbackup/ | jq -r -s 'sort_by(.lastModified) | last | .key' | xargs -I{} mc cp postgres_backup/${S3_BUCKET}/pgbackup/{} latest_postgres_backup.sql.gz
