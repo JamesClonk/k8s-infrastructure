c779d269f00c0bc925468b7146c476ef575c971f
JamesClonk <jamesclonk@jamesclonk.ch>
2026-03-18 22:25:21 +0100

    add ondemand create-backup script

--- templates/postgres/restore_backup.sh
+++ templates/postgres/restore_backup.sh
@@ -5,6 +5,6 @@ set -o pipefail
 source ../setup.sh
 
 # import
-echo "import backup from [postgres_backup.sql] into [postgres] database ..."
+echo "importing backup from [postgres_backup.sql] into [postgres] database ..."
 #kubectl exec -i -n postgres postgres-0 -- env PGPASSWORD="{{{ .postgres.password }}}" psql -U postgres -h 127.0.0.1 -p 6432 < postgres_backup.sql
 gunzip -c postgres_backup.sql.gz | kubectl exec -i -n postgres postgres-0 -- env PGPASSWORD="{{{ .postgres.password }}}" psql -U postgres -h 127.0.0.1 -p 6432
